b6fc0fa99ff283e027ec409e0c1c12090ec7010c
MARTI <MARTI@MARTINX>
2023-08-18 13:55:12 -0600

    VERSION 1

---
+++ src/main/java/com/mycompany/examen_1/MAIN.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.examen_1;
+
+/**
+ *
+ * @author MARTI
+ */
+public class MAIN {
+    public static void main(String[] args) {
+        
+        MENU NEW= new MENU();
+        
+        NEW.setVisible(true);
+        NEW.setLocationRelativeTo(NEW);
+        
+        
+        
+    }
+}
