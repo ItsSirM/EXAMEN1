e2af4bac9bf9c498ab20d0432586b4168ae0f570
pcast <pcast@DESK-E>
2023-08-18 16:49:08 -0600

    uber done

--- src/main/java/com/mycompany/examen_1/UberSocial.java
+++ src/main/java/com/mycompany/examen_1/UberSocial.java
@@ -57,11 +57,20 @@ public class UberSocial {
         }
     }
     
-    public void agregarComment(String user, int postID, String autor, String comment){
-        red = "FACEBOOK";
-        SocialClass usuario = buscar(user);
-        
+    public void agregarComment(String user, int postID, String author, String comment) {
+        SocialClass acc=buscar(user);
+        if (acc instanceof Facebook) {
+            Comment n = new Comment(postID, author, comment);
+            if (((Facebook) acc).addComment(n)) {
+                System.out.println("Comentario añadido");
+            } else {
+                System.out.println("Post no valido");
+            }
+        } else {
+            System.out.println("No valido");
+        }
     }
+        
     
     public void profileForm(String user){
         SocialClass usuario = buscar(user);
@@ -70,33 +79,3 @@ public class UberSocial {
         }
     }
 }
-/*
-Crear una clase llamada UberSocial la cual tiene un atributo de tipo arraylist para manejar clases de redes sociales.
-Tiene las siguientes funciones:
-
-buscar(string username) Busca de manera recursiva y retorna el objeto de la cuenta que tenga dicho username.
-Si no existe, se retorna null. (5%)
-
-void agregarCuenta(string username, String tipo).
-Se crea la cuenta para agregarla en el arraylist según el tipo “FACEBOOK” o “TWITTER”. 
-El username DEBE SER UNICO. (10%)
-
-void agregarPost(String user, String post). 
-El user debe de existir, si existe, se agrega el post. (5%)
-
-void agregarAmigo(string user1, String user2). 
-Se quiere agregar el user2 como amigo del user1.
-Ambas deben existir y ser del mismo tipo de cuenta, 
-es decir Facebook user agrega Facebook user. 
-Si la cuenta de ambos es de FACEBOOK, se agregan ambos como amigos. (10%)
-
-void agregarComment(string user, int postID, string autor, string comment).
-La red social con ese user debe existir y debe ser de FACEBOOK.
-Se recibe los demás valores del comentario.
-Nota: No es necesario validar que el autor exista también, (10%)
-
-void profileFrom(String user). 
-Imprime el contenido del profile del usuario que se recibe de parámetro.
-Este debe existir. (5%)
-Que pruebe las funciones de la b-f. (5%)
-*/
