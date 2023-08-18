96aded8ef0675e33c322d4e88112a1b6d16c9a01
hp <hp@LAPTOP-CLAUDIO>
2023-08-18 14:41:30 -0600

    faceboo

--- src/main/java/com/mycompany/examen_1/Facebook.java
+++ src/main/java/com/mycompany/examen_1/Facebook.java
@@ -1,4 +1,8 @@
 package com.mycompany.examen_1;
+import java.util.ArrayList;
 public class Facebook extends SocialClass implements Commentable {
-    private ArrayList
+    private ArrayList<Comment>;
+    
+    
+    
 }
